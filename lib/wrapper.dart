1497eb4d845615937621cda441ebd633dfffd1f5
melikaksu <melik.aksu@gmail.com>
2020-04-29 01:13:24 +0300

    master

--- lib/wrapper.dart
+++ lib/wrapper.dart
@@ -1,33 +1,33 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flare_flutter/flare_actor.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_spinkit/flutter_spinkit.dart';
 import 'package:homesweethome/screens/login.dart';
 import 'package:homesweethome/screens/home.dart';
-
 
 class Wrapper extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-       return StreamBuilder(
-       stream: FirebaseAuth.instance.onAuthStateChanged,
-       builder: (BuildContext context, AsyncSnapshot<FirebaseUser> snapshot) {
-
+    return StreamBuilder(
+      stream: FirebaseAuth.instance.onAuthStateChanged,
+      builder: (BuildContext context, AsyncSnapshot<FirebaseUser> snapshot) {
         //  var at= snapshot.data;
-         
-        if(snapshot.connectionState==ConnectionState.waiting){
-           return SpinKitWave(
-             color: Colors.deepOrange,
-             size: 40,
-           );
-         }
-         if(!snapshot.hasData || snapshot.data==null){
-           return Login();
-         }
-         return   MyHomePage();
 
-     },);
-
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return Center(
+            child: FlareActor(
+              "assets/circular.flr",
+              animation: "circular",
+              fit: BoxFit.contain,
+            ),
+          );
+        }
+        if (!snapshot.hasData || snapshot.data == null) {
+          return Login();
+        }
+        return MyHomePage();
+      },
+    );
 
 //    if(user == null){
 //      return
@@ -35,6 +35,5 @@ class Wrapper extends StatelessWidget {
 //    else{
 //
 //    }
-
   }
 }
