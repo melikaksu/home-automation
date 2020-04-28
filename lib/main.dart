094df47935368fa85cdb7e8ea848ff470bf57b60
melikaksu <melik.aksu@gmail.com>
2020-04-28 13:36:05 +0300

    master

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
       value: AuthService().user,
       child: MaterialApp(
         debugShowCheckedModeBanner: false,
-        title: 'Flutter Demo',
+        title: 'Giderler ve Alışveriş Listessi ',
         theme: ThemeData(
             cursorColor: Colors.black,
             //brightness: Brightness.dark,
