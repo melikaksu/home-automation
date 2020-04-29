e97cff5d44962a4156906248805ab8cea283faa2
melikaksu <melik.aksu@gmail.com>
2020-04-29 22:45:26 +0300

    master

--- lib/services/firestore.dart
+++ lib/services/firestore.dart
@@ -1,8 +1,12 @@
 import 'dart:core';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:homesweethome/models/avatar_reference.dart';
 import 'package:homesweethome/models/outgoing.dart';
 import 'package:homesweethome/services/auth.dart';
 import 'dart:async';
+
+import 'package:homesweethome/services/firestore_path.dart';
 
    var at = AuthService();
 
@@ -23,6 +27,12 @@ import 'dart:async';
 // final CollectionReference myCollection=Firestore.instance.collection('list');
 
  class FirestoreService {
+   final String uid;
+   FirestoreService([this.uid]):assert(uid!=null);
+   
+//  FirestoreService({ @required this.uid}) : assert(uid != null);
+  
+
 
     String value;
     static String col=" ";
@@ -79,6 +89,21 @@ import 'dart:async';
     return snapshots;
   }
 
-  
+
+  // Sets the avatar download url
+  Future<void> setAvatarReference(AvatarReference avatarReference) async {
+    final path = FirestorePath.avatar(uid);
+    final reference = Firestore.instance.document(path);
+    await reference.setData(avatarReference.toMap());
+  }
+
+  // Reads the current avatar download url
+   Stream<AvatarReference> avatarReferenceStream() {
+    final path = FirestorePath.avatar(uid);
+    final reference = Firestore.instance.document(path);
+    final snapshots = reference.snapshots();
+    return snapshots.map((snapshot) => AvatarReference.fromMap(snapshot.data));
+  }
+
 
 }
